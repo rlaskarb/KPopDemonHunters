7f7fd78e659f097507aa4a825a89375743073b1a
NAMKYU\rlask <rlaskarb20@gmail.com>
2025-09-22 17:17:49 +0900

    9/22

--- media/js/sub_js/sub_img_back.js
+++ media/js/sub_js/sub_img_back.js
@@ -1,33 +1,33 @@
 window.addEventListener("DOMContentLoaded", function () {
-	const urlParams = new URLSearchParams(window.location.search);
-	const value = Number(urlParams.get("num"));
-	const contentElement = document.querySelector("#content");
-	const imgBGElement = this.document.querySelector("#imgBG");
-	const downButton = this.document.querySelector(".down");
+  const urlParams = new URLSearchParams(window.location.search);
+  const value = Number(urlParams.get("num"));
+  const contentElement = document.querySelector("#content");
+  const imgBGElement = this.document.querySelector("#imgBG");
+  const downButton = this.document.querySelector(".down");
 
-	//  화면 크기에 따라 레이아웃 설정
-	function setScreenLayout() {
-		const screenWidth = window.innerWidth;
-		const screenHeight = window.innerHeight;
+  //  화면 크기에 따라 레이아웃 설정
+  function setScreenLayout() {
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
 
-		contentElement.style.marginTop = screenHeight + "px";
-		if (screenWidth > 855) {
-			imgBGElement.src = "./images/sub" + value + "_big.jpg";
-		} else {
-			imgBGElement.src = "./images/sub" + value + "_small.jpg";
-		}
-	}
+    contentElement.style.marginTop = screenHeight + "px";
+    if (screenWidth > 855) {
+      imgBGElement.src = "./images/sub" + value + "/sub" + value + "_big.jpg";
+    } else {
+      imgBGElement.src = "./images/sub" + value + "/sub" + value + "_small.jpg";
+    }
+  }
 
-	setScreenLayout();
+  setScreenLayout();
 
-	window.addEventListener("resize", setScreenLayout);
-	downButton.addEventListener("click", function (e) {
-		e.preventDefault();
-		const screenHeight = window.innerHeight;
+  window.addEventListener("resize", setScreenLayout);
+  downButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const screenHeight = window.innerHeight;
 
-		window.scrollTo({
-			top: screenHeight,
-			behavior: "smooth",
-		});
-	});
+    window.scrollTo({
+      top: screenHeight,
+      behavior: "smooth",
+    });
+  });
 });
